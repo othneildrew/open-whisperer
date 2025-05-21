e56960e4820609a3b732a8a7b0d60e329af6a6a5
Othneil Drew <codeguydrew@gmail.com>
2025-05-21 08:22:34 -0500

    chore: update app configs

--- apps/web-ui/next.config.ts
+++ apps/web-ui/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const NEXT_SERVER_ENDPOINT = process.env.NEXT_SERVER_ENDPOINT || 'http://localhost:8000';
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL('http://localhost:8000/media/**'),
+      new URL(`${NEXT_SERVER_ENDPOINT}/media/**`),
     ]
-  }
+  },
 };
 
 export default nextConfig;
